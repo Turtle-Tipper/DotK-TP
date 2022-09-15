7b0784bc717217cd27f9b6c55bb5d68c8fcbfb6a
Turtle-Tipper <99361666+Turtle-Tipper@users.noreply.github.com>
2022-09-15 16:49:58 -0700

    refactoring attributes to composition
    - created AttributeComponent to handle attributes
    - should reduce the need for complex inheritance structure to determine which attributes different characters have

---
+++ Source/DeadOfTheKnightTP/DotK_CharacterAttributeComponent.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DotK_CharacterAttributeComponent.h"
+
+// Sets default values for this component's properties
+UDotK_CharacterAttributeComponent::UDotK_CharacterAttributeComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDotK_CharacterAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDotK_CharacterAttributeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
