69276d84761181ebc7c9f781051a5aa8c1f8d1df
Turtle-Tipper <99361666+Turtle-Tipper@users.noreply.github.com>
2022-10-04 23:22:45 -0700

    replaced CharacterAttributeComponent
    - moved everything into AttributeComponent and deleted CharacterAttributeComponent as Attributes will also exist on Equipment

--- Source/DeadOfTheKnightTP/DotK_CharacterAttributeComponent.h
+++ Source/DeadOfTheKnightTP/DotK_CharacterAttributeComponent.h
@@ -6,16 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "DotK_CharacterAttributeComponent.generated.h"
 
-UENUM(BlueprintType)
-enum class ECharacterStat : uint8
-{
-	None			UMETA(DisplayName = "None"),
-	Constitution	UMETA(DisplayName = "Constitution"),
-	Strength		UMETA(DisplayName = "Strength"),
-	Agility			UMETA(DisplayName = "Agility"),
-	Intellect		UMETA(DisplayName = "Intellect"),
-	Wisdom			UMETA(DisplayName = "Wisdom"),
-};
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class DEADOFTHEKNIGHTTP_API UDotK_CharacterAttributeComponent : public UActorComponent
@@ -26,45 +17,7 @@ public:
 	// Sets default values for this component's properties
 	UDotK_CharacterAttributeComponent();
 
-	/* Variable that keeps track of currently available skill points. */
-	UPROPERTY(EditAnywhere, Category = "Leveling")
-	int AvailableSkillPoints = 1;
-
-	// ** PRIMARY STATS ** //
-
-	//EPrimaryStat PrimaryStats;
-
-	TArray<uint8> PrimaryStatArray;
-
-	/* Armor reduces damage taken. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int ArmorValue;
-
-	/* Constitution modifies HP and block value. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int ConstitutionValue;
-
-	/* Strength modifies physical damage. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int StrengthValue;
-
-	/* Agility modifies attack speed and critical strike chance. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int AgilityValue;
-
-	/* Intellect modifies spell damage. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int IntellectValue;
-
-	/* Wisdom modifies maximum mana and mana regen. */
-	UPROPERTY(EditAnywhere, Category = "Primary Stats")
-	int WisdomValue;
-
-	// ** SECONDARY STATS ** //
-
-	/* Haste modifies casting speed. */
-	UPROPERTY(EditAnywhere, Category = "Secondary Stats")
-	float HasteValue;
+	
 
 protected:
 	// Called when the game starts
@@ -75,38 +28,6 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 
-	// ** STATS FUNCTIONS ** //
-
-	/* Called to permanently increase a stat. On level up or other progression points. */
-	UFUNCTION(BlueprintCallable)
-	void IncreaseStat(ECharacterStat StatToIncrease, int IncreaseValue);
-
-	/* Called to temporarily increase a stat. */
-	UFUNCTION(BlueprintCallable)
-	void BuffStat();
-
-	/* Called to temporarily debuff a stat. */
-	UFUNCTION(BlueprintCallable)
-	void DebuffStat();
-
-	/* GETTER FUNCTIONS */
-
-	UFUNCTION(BlueprintPure)
-	int GetAvailableSkillPoints() { return AvailableSkillPoints; }
-
-	/* STATS */
-
-	UFUNCTION(BlueprintPure)
-	int GetConstitutionValue() { return ConstitutionValue; }
-	UFUNCTION(BlueprintPure)
-	int GetStrengthValue() { return StrengthValue; }
-	UFUNCTION(BlueprintPure)
-	int GetAgilityValue() { return AgilityValue; }
-	UFUNCTION(BlueprintPure)
-	int GetIntellectValue() { return IntellectValue; }
-	UFUNCTION(BlueprintPure)
-	int GetWisdomValue() { return WisdomValue; }
-	UFUNCTION(BlueprintPure)
-	int GetHasteValue() { return HasteValue; }
+	
 		
 };
