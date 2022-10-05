7e189b16c67d6d639eb676d2ad620ffa417fe3e2
Turtle-Tipper <99361666+Turtle-Tipper@users.noreply.github.com>
2022-10-05 00:40:08 -0700

    created weapon and equipment base class
    - created weapon and equipment base class
    - weapons and equipment will both have attribute components and durability components
    - weapon will have attack speed, damage, weapon type, and equip slot
    - equipment will have armor, equip slot, stam drain modifier, movement speed modifier, and noise modifer

---
+++ Source/DeadOfTheKnightTP/DOTK_EquipmentBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DOTK_ItemBase.h"
+#include "DOTK_EquipmentBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADOFTHEKNIGHTTP_API ADOTK_EquipmentBase : public ADOTK_ItemBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DeadOfTheKnightTP/DOTK_WeaponBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DOTK_ItemBase.h"
+#include "DOTK_WeaponBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADOFTHEKNIGHTTP_API ADOTK_WeaponBase : public ADOTK_ItemBase
+{
+	GENERATED_BODY()
+	
+};
