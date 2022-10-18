728a3646180ada90437d23e611efc126ae13d6d0
Turtle-Tipper <99361666+Turtle-Tipper@users.noreply.github.com>
2022-10-18 15:17:55 -0700

    instantiated EnemyBase class
    - created EnemyBase class as child of DeadOfTheKnightTPCharacter

---
+++ Source/DeadOfTheKnightTP/DOTK_EnemyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DeadOfTheKnightTPCharacter.h"
+#include "DOTK_EnemyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADOFTHEKNIGHTTP_API ADOTK_EnemyBase : public ADeadOfTheKnightTPCharacter
+{
+	GENERATED_BODY()
+	
+};
